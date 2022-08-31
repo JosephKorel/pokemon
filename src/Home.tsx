5f029193abd80de7fbc857777beef72840938be8
José Carlos Rolim Etchepare <jcjosecarlos03@gmail.com>
2022-08-30 22:32:14 -0300

    add responsive design

--- src/Home.tsx
+++ src/Home.tsx
@@ -54,7 +54,13 @@ function Home() {
       )
     : pokemonList;
 
-  const PokemonCard = ({ pokemon }: { pokemon: Pokemon }): JSX.Element => {
+  const PokemonCard = ({
+    pokemon,
+    key,
+  }: {
+    pokemon: Pokemon;
+    key: number;
+  }): JSX.Element => {
     const oneAbility = pokemon.abilities.length === 1 ? true : false;
 
     //Insere vírgula ou ponto nas habilidades
@@ -66,16 +72,20 @@ function Home() {
       }
     };
     return (
-      <div className=" bg-[#333333] font-poppins rounded-md flex flex-col justify-center items-center py-2">
+      <div
+        key={key}
+        className=" bg-[#333333] font-poppins rounded-md flex flex-col justify-center items-center py-2"
+      >
         <img src={pokemon.avatar} className="w-[184px]"></img>
         <h1 className="text-white font-medium  text-lg">
           {pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}
         </h1>
-        <p className="text-white text-xs">
+        <p className="text-white text-xs text-center">
           Abilities:{" "}
           {pokemon.abilities.map((skill, index) => (
             <>
-              {skill.ability.name}
+              {skill.ability.name[0].toUpperCase() +
+                skill.ability.name.slice(1)}
               {marks(index)}
             </>
           ))}
@@ -85,59 +95,72 @@ function Home() {
   };
 
   return (
-    <div>
-      {pokemonList.length > 0 ? (
+    <div className="h-screen">
+      {pokemonList.length > 0 && (
         <div>
           <div className="bg-[#002E57] pt-6 pb-10 w-full flex justify-center">
             <img
-              className="w-[368px] inline-block"
+              className="w-[250px] lg:w-[368px] inline-block"
               src="./pokemon_logo.png"
             ></img>
           </div>
-          <div className="flex justify-center">
-            <div className="flex justify-center gap-5 absolute -translate-y-7 w-1/3 text-gray-800 p-2 bg-[#D0CBCB] rounded-md px-3">
-              <input
-                placeholder="Pesquisar por pokémon"
-                value={search}
-                onChange={(e) => setSearch(e.currentTarget.value)}
-                className="w-full bg-[#D0CBCB] text-lg rounded-md focus:outline-none appearance-none"
-              />
-              <img src="./search_icon.png" className="w-10"></img>
+          <main>
+            <div className="flex justify-center">
+              <div className="flex justify-center gap-5 absolute -translate-y-7 w-11/12 md:w-2/3 xl:w-1/2 2xl:w-1/3 text-gray-800 p-2 bg-[#D0CBCB] rounded-md px-3 hover:border-indigo-800 border duration-200">
+                <input
+                  placeholder="Pesquisar por pokémon"
+                  value={search}
+                  onChange={(e) => setSearch(e.currentTarget.value)}
+                  className="w-full bg-[#D0CBCB] text-base lg:text-lg rounded-md focus:outline-none appearance-none"
+                />
+                <img src="./search_icon.png" className="w-6 lg:w-10"></img>
+              </div>
             </div>
-          </div>
-          <div className="flex justify-between w-2/3 m-auto mt-16">
-            <div className="flex items-center gap-3 ml-2 ">
-              <h2 className="font-bold text-[#2B74B8] font-poppins">
-                Total de Pokemons
-              </h2>
-              <p className="px-3 font-bold text-sm bg-[#333333] rounded-full text-gray-200 font-inter">
-                {pokemonList.length}
-              </p>
+            <div className="flex flex-col md:flex-row md:justify-between items-center w-full md:w-5/6 2xl:w-[64%] m-auto mt-8 lg:mt-16">
+              <div className="flex items-center gap-3">
+                <h2 className="font-bold text-[#2B74B8] font-poppins text-lg">
+                  Total de Pokemons
+                </h2>
+                <p className="px-3 font-bold text-xs bg-[#333333] rounded-full text-gray-200 font-inter">
+                  {pokemonList.length}
+                </p>
+              </div>
+              <div className="flex items-center gap-3">
+                <h2 className="font-bold text-[#2B74B8] font-poppins text-lg">
+                  Exibindo
+                </h2>
+                <p className="px-3 font-bold text-xs bg-[#333333] rounded-full text-gray-200 font-inter">
+                  {pokemonSearch.length}
+                </p>
+                <h2 className="font-bold text-[#2B74B8] font-poppins text-lg">
+                  de
+                </h2>
+                <p className="px-3 font-bold text-xs bg-[#333333] rounded-full text-gray-200 font-inter">
+                  {pokemonList.length}
+                </p>
+              </div>
             </div>
-            <div className="flex items-center gap-3 mr-2">
-              <h2 className="font-bold text-[#2B74B8] font-poppins">
-                Exibindo
-              </h2>
-              <p className="px-3 font-bold text-sm bg-[#333333] rounded-full text-gray-200 font-inter">
-                {pokemonSearch.length}
-              </p>
-              <h2 className="font-bold text-[#2B74B8] font-poppins">de</h2>
-              <p className="px-3 font-bold text-sm bg-[#333333] rounded-full text-gray-200 font-inter">
-                {pokemonList.length}
-              </p>
-            </div>
-          </div>
-          <div className="bg-[#1A1A1A] w-2/3 m-auto mt-12 grid grid-rows-2 grid-cols-5 gap-5">
-            {pokemonSearch.map((pokemon) => (
-              <PokemonCard pokemon={pokemon} />
-            ))}
-          </div>
-        </div>
-      ) : (
-        <div>
-          <h1>Carregando pokémons</h1>
+            {pokemonSearch.length > 0 ? (
+              <>
+                <div className="w-full p-2 lg:p-0 md:w-11/12 2xl:w-2/3 m-auto mt-6 lg:mt-12 grid grid-rows-5 grid-cols-2 lg:grid-rows-2 lg:grid-cols-5 gap-5">
+                  {pokemonSearch.map((pokemon, index) => (
+                    <PokemonCard pokemon={pokemon} key={index} />
+                  ))}
+                </div>
+              </>
+            ) : (
+              <div className="text-center mt-10">
+                <h1 className="text-2xl text-gray-100 font-inter font-bold">
+                  Nenhum pokémon encontrado
+                </h1>
+              </div>
+            )}
+          </main>
         </div>
       )}
+      <footer className="py-2 2xl:absolute 2xl:bottom-2 text-xs text-white font-poppins text-center w-full">
+        Todos os direitos reservados a Campolink 2022.
+      </footer>
     </div>
   );
 }
